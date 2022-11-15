7133193ed1ad7e89fae8975ac231f429939b185c
Fausto Obama <Faustoobama@outlook.com>
2022-11-15 21:45:34 +0100

    ok

--- Parrafo.php
+++ Parrafo.php
@@ -3,7 +3,7 @@
         function isValid(){
             if((strlen($this->getValue()) > 120) && preg_match('/^[a-zA-Záéí óúüöäñ0-9.\,\:ç\s]{120,400}$/iu', $this->getValue())) {
                 return ['outcome' => true, 'message' => parent::isvalid()['message']];
-            }else return ['outcome' => false, 'message' => parent::isValid()['message'].'<br>El párrafo contiene caracteres no válido o longitud inválida'];
+            }else return ['outcome' => false, 'message' => parent::isValid()['message'].'<br>El párrafo contiene caracteres no válidos o longitud inválida'];
         }
     }
 ?>
