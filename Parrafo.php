73df898180dc890532af98a7a603c5bed1b3f424
Fausto Obama <fngomo@evicertia.com>
2022-11-16 06:00:54 +0100

    Repo Becarios tarea en grupo

--- Parrafo.php
+++ Parrafo.php
@@ -1,5 +1,5 @@
 <?php
-    class Parrafo extends Texto { //¿necesario poner un placeholder para indicar la longitud?
+    class Parrafo extends Texto {
         function isValid(){
             if((strlen($this->getValue()) > 120) && preg_match('/^[a-zA-Záéí óúüöäñ0-9.\,\:ç\s]{120,400}$/iu', $this->getValue())) {
                 return ['outcome' => true, 'message' => parent::isvalid()['message']];
